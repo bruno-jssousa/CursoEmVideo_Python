d7bc8fc207543d445858561beed6ea851c9ef399
Bruno <brunothoresen@gmail.com>
2022-12-12 22:11:45 -0300

    ordenação

--- Ex005.py
+++ Ex005.py
@@ -8,6 +8,6 @@ print(' O antecessor de {} é {} e o sucessor é {}:'.format(n,n-1,n+1))
 print('\n')
 print(f'O dobro de {n} é : {n*2} \n'
       f'O triplo de {n} é : {n*3} \n'
-      f'A raiz quadrada  de {n} é : {pow(n,1/2)} \n'
+      f'A raiz quadrada  de {n} é : {pow(n,1/2):.2f} \n'
 
 )
